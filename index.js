01948d9d8b62e50b86d2d404ad2ce3de9548dea3
ThanadolU <dolthanadolu@gmail.com>
2023-04-15 23:53:08 +0700

    🚩 finished registration system

--- index.js
+++ index.js
@@ -4,21 +4,32 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const expressSession = require('express-session');
 const flash = require('connect-flash');
+require('dotenv').config()
+
+// MongoDB Connection
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PWD}@cluster0.wprzzbg.mongodb.net/?retryWrites=true&w=majority`, {
+    useNewUrlParser: true
+})
 
 // Controllers
 const indexController = require('./controllers/indexController')
 const loginController = require('./controllers/loginController')
 const registerController = require('./controllers/registerController')
+const storeController = require('./controllers/storeUserController')
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(flash());
+app.use(expressSession({
+    secret: "node secret"
+}))
 app.set('view engine', 'ejs');
 
 app.get('/', indexController);
 app.get('/login', loginController);
 app.get('/register', registerController);
+app.post('/user/register', storeController);
 
 app.listen(4000, () => {
     console.log("App listening on port 4000");
